efb5373d4f6ab11a261b07a0f8405852d7469bc3
celine1799 <celine.turlot2020@gmail.com>
2024-12-03 16:13:30 +0100

    add css login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import loginImage from '../img/login.png';
 
 function Login({ onLogin }) {
   const [username, setUsername] = useState('');
@@ -11,17 +12,19 @@ function Login({ onLogin }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="login-form">
+    <div className="container">
+      <img src={loginImage} alt="Logo Pokémon" className="pokemon" />
+      <form onSubmit={handleSubmit} className="login-form">
       <h2>Connexion Pokémon</h2>
       <input
         type="text"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Entrez votre nom d'utilisateur"
-        required
-      />
+        required/>
       <button type="submit">Se connecter</button>
-    </form>
+      </form>
+    </div> 
   );
 }
 
